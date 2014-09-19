2f5013ce1ffe5de35d4714d877a491b0d60ff3fe
Adrien Neveu <adrien.neveu@epita.fr>
2014-09-19 14:35:01 +0200

    Fixed compile file

--- src/interface/main.c
+++ src/interface/main.c
@@ -9,12 +9,12 @@ void Gtk_Initialize(int argc, char *argv[])
 	GtkWidget *window;
 
 	gtk_init(&argc, &argv);
-	
+
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title(GTK_WINDOW(window), "window");
 
 	g_signal_connect(window, "destroy", gtk_main_quit, NULL);
-	
+
 	gtk_widget_show(window);
 
 	gtk_main();

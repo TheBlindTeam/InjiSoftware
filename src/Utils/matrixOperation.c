e9bccbdb9c5e76ff5787afa869a9b7f66184c1b3
Adrien Neveu <adrien.neveu@epita.fr>
2014-10-08 09:49:35 -0700

    Fixed some errors from matrixOperations. Still not compiling guys....

--- src/Utils/matrixOperation.c
+++ src/Utils/matrixOperation.c
@@ -1,6 +1,6 @@
-#include <matrixOperation.h>
+#include "matrixOperation.h"
 
-UMultiply(**Pixel matrix, **double convolution, int matrixSize)
+Pixel** UMultiply(**Pixel matrix, **double convolution, int matrixSize)
 {
 	Pixel tmp[matrixSize][matrixSize];
 
@@ -8,18 +8,21 @@ UMultiply(**Pixel matrix, **double convolution, int matrixSize)
 	{
 		for(int x = 0; x < matrix.Size; x++)
 		{
-			tmp[x][y] += matrix[x][y].r * (guchar)convolution[y][x];
-			tmp[x][y] += matrix[x][y].g * (guchar)convolution[y][x];
-			tmp[x][y] += matrix[x][y].b * (guchar)convolution[y][x];
+			tmp[x][y] += matrix[x][y].r *
+				(guchar)convolution[y][x];
+			tmp[x][y] += matrix[x][y].g *
+				(guchar)convolution[y][x];
+			tmp[x][y] += matrix[x][y].b *
+				(guchar)convolution[y][x];
 		}
 	}
 
 	return tmp;
 }
 
-UExtract(**Pixel matrix, int matrixSize, int size, int pos_x, int pos_y)
+Pixel** UExtract(**Pixel matrix, int matrixSize, int size, int pos_x,
+	int pos_y)
 {
-
 	Pixel tmp[size][size];
 	Pixel defaultPix;
 	defaultPix.r = 255;
@@ -31,7 +34,8 @@ UExtract(**Pixel matrix, int matrixSize, int size, int pos_x, int pos_y)
 	{
 		for(int x = pos_x; x < pos_x + size; x++)
 		{
-			if((x >= 0 && x < matrixSize) && (y >= 0 && y < matrixSize))
+			if(x >= 0 && x < matrixSize &&
+					y >= 0 && y < matrixSize)
 				tmp[x][y] = matrix[x][y];
 			else
 				tmp[x][y] = defaultPix;
@@ -39,12 +43,11 @@ UExtract(**Pixel matrix, int matrixSize, int size, int pos_x, int pos_y)
 	}
 
 	return tmp;
-
 }
 
-UConvolution(**Pixel matrix, **double convolution, int size, int matrixSize)
+Pixel** UConvolution(**Pixel matrix, **double convolution, int size, 
+	int matrixSize)
 {
-
 	Pixel result[size][size];
 	Pixel subMatrix[matrixSize][matrixSize];
 	
@@ -52,10 +55,12 @@ UConvolution(**Pixel matrix, **double convolution, int size, int matrixSize)
 	{
 		for(int x = 0; x < matrixSize; x++)
 		{
-				subMatrix = UExtract(matrix, size, matrixSize, x, y);
-				subMatrix = UMultiply(subMatrix, convolution, matrixSize);
+			subMatrix = UExtract(matrix, size, matrixSize, x, y);
+			subMatrix = UMultiply(subMatrix, convolution,
+				matrixSize);
 
-				result[x][y] = subMatrix[matrixSize / 2][matrixSize / 2];
+			result[x][y] = subMatrix
+				[matrixSize / 2][matrixSize / 2];
 		}
 	}
 
--- src/Utils/matrixOperation.h
+++ src/Utils/matrixOperation.h
@@ -1,11 +1,12 @@
-#include<image.h>
+#include "image.h"
 
-// David : Multiply Pixel Matrix By Convolution Matrix
+// David: Multiply Pixel Matrix By Convolution Matrix
 Pixel** UMultiply(**Pixel matrix, **double convolution, int matrixSize);
 
-// David : Extract Submatrix From The Top Left Corner
-Pixel** UExtract(**Pixel matrix,
-int matrixSize, int extractSize, int pos_x, int pos_y);
+// David: Extract Submatrix From The Top Left Corner
+Pixel** UExtract(**Pixel matrix, int matrixSize, int extractSize, int pos_x,
+	int pos_y);
 
-// David : Apply A Convulution Matrix On Every Pixel
-Pixel** UConvolution(**Pixel matrix, **double convolution, int size ,int matrixSize);
+// David: Apply A Convulution Matrix On Every Pixel
+Pixel** UConvolution(**Pixel matrix, **double convolution, int size,
+	int matrixSize);

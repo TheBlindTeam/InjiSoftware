e5d55467e47269219c9b19c4d8255355f1529a12
David Peicho <dadouvic2@hotmail.fr>
2014-10-11 09:29:36 -0700

    matrixOperation debug

--- src/Utils/matrixOperation.h
+++ src/Utils/matrixOperation.h
@@ -1,14 +1,16 @@
 #include "image.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 // David: Multiply Pixel Matrix By Convolution Matrix
-Pixel** UMultiply(**Pixel matrix, **double convolution, int matrixSize);
+Pixel** UMultiply(Pixel **matrix, double **convolution, int matrixSize);
 
-// David: Extract Submatrix From The Top Left Corner
-Pixel** UExtract(**Pixel matrix, int matrixSize, int extractSize, int pos_x,
+// David: Extract Submatrix From The Center
+Pixel** UExtract(Pixel **matrix, int matrixSize, int extractSize, int pos_x,
 	int pos_y);
 
 // David: Apply A Convulution Matrix On Every Pixel
-Pixel** UConvolution(**Pixel matrix, **double convolution, int size,
+Pixel** UConvolution(Pixel **matrix, double **convolution, int size,
 	int matrixSize);
 
 

d697a7a12cbdda274a74ed5fda7d8cfe619260ca
leandr_a <aurelien.leandri@epita.fr>
2014-10-18 23:34:16 +0200

    Ajout de la partie découpage, seuls manquent le découpage des blocs et mots

---
+++ src/Utils/split.c
@@ -0,0 +1,287 @@
+#include "split.h"
+#include "image.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/*Nota bene : 
+J'ai considéré l'image comme ayant un repère d'axe x VERTICAL orienté vers 
+le bas et d'axe y HORIZONTAL orienté vers la gauche*/
+
+List Enqueue(List list, Box b, int * count)
+{
+	ElementList * elt = malloc(sizeof(ElementList));
+	elt->value = b;
+	elt->next = list;
+	(*count)++;
+	printf("valeur de count : %d\n", *count);
+	return elt;
+}
+
+Box * ListToArray(List list, int count)
+{
+	ElementList * temp = list;
+	Box * b = malloc(sizeof(Box)*count);
+	int i = count - 1;
+	while(i >= 0 && temp != NULL)
+	{
+		b[i] = temp->value;
+		temp = temp->next;
+		i--;
+	}
+	return b;
+}
+
+
+
+//#flemmeDeDeplacerLeCode
+ImageGS BuildImageGS(int width, int height);
+int VerticalSpaceInfo(ImageGS img, Box b, guchar c);
+
+
+
+int HorizontalSpaceInfo(ImageGS img, Box b, guchar c)
+/*
+Renvoie la moyenne des espaces entre les lignes, sans prendre en compte les espaces
+aux extrémités (qui peuvent être très grands).
+*/
+{
+	int nbSpaces = 0, total = 0, firstSpace;
+	int inLine = 0;
+	int space = 0;
+	for (int i = b.rectangle.y; i < b.rectangle.y + b.rectangle.height; i++)
+	{
+		int j = b.rectangle.x;
+		while (j < b.rectangle.x + b.rectangle.width && img.intensity[i][j] >= c)
+			j++;
+		if (!inLine && j != b.rectangle.width)
+			inLine = 1;
+		if (inLine && j == b.rectangle.width)
+		{
+			inLine = 0;
+			firstSpace = i;
+		}
+	}
+
+	for (int i = firstSpace; i < b.rectangle.y + b.rectangle.height; i++)
+	{
+		int j = b.rectangle.x;
+		while (j < b.rectangle.x + b.rectangle.width && img.intensity[i][j] >= c)
+			j++;
+		if (!inLine && j != b.rectangle.width)
+			inLine = 1;
+			total += space;
+			space = 0;
+			nbSpaces++;
+		if (inLine && j == b.rectangle.width)
+		{
+			inLine = 0;
+			space++;
+		}
+	}
+	return total / nbSpaces;
+}
+
+
+
+int VerticalSpaceInfo(ImageGS img, Box b, guchar c)
+{
+	int nbSpaces = 0, total = 0, firstSpace;
+	int inLine = 0;
+	int space = 0;
+	for (int i = b.rectangle.x; i < b.rectangle.x + b.rectangle.width; i++)
+	{
+		int j = b.rectangle.y;
+		while (j < b.rectangle.y + b.rectangle.height && img.intensity[j][i] >= c)
+			j++;
+		if (!inLine && j != b.rectangle.height)
+			inLine = 1;
+		if (inLine && j == b.rectangle.height)
+		{
+			inLine = 0;
+			firstSpace = i;
+		}
+	}
+
+	for (int i = firstSpace; i < b.rectangle.x + b.rectangle.width; i++)
+	{
+		int j = b.rectangle.y;
+		while (j < b.rectangle.y + b.rectangle.height && img.intensity[j][i] >= c)
+			j++;
+		if (!inLine && j != b.rectangle.height)
+			inLine = 1;
+			total += space;
+			space = 0;
+			nbSpaces++;
+		if (inLine && j == b.rectangle.width)
+		{
+			inLine = 0;
+			space++;
+		}
+	}
+	return total / nbSpaces;
+}
+
+
+
+Box SplitChars(ImageGS img, Box b, guchar c)
+{
+	int count = 0;
+	List listBoxes = NULL;
+	printf("Initialization list\n");
+	int inLine = 0; 
+	for (int j = b.rectangle.x; j < b.rectangle.x + b.rectangle.width; j++)
+	{
+		int i = b.rectangle.y;
+		//NOTE : structure d'image en grey scale à faire
+		while (i < b.rectangle.y + b.rectangle.height && img.intensity[i][j] >= c)
+			i++;
+		if (!inLine && i != b.rectangle.y + b.rectangle.height)
+		{
+			inLine = 1;
+			Box btemp;
+			btemp.rectangle.x = j;
+			btemp.rectangle.y = b.rectangle.y;
+			btemp.rectangle.height = b.rectangle.height;
+			listBoxes = Enqueue(listBoxes, btemp, &count);
+			//printf("valeur x : %d\n", listBoxes->value.rectangle.x);
+		}
+		if (inLine && i == b.rectangle.y + b.rectangle.height)
+		{
+			inLine = 0;
+			listBoxes->value.rectangle.width = j - listBoxes->value.rectangle.x;
+			PrintBoxInformations(listBoxes->value);
+		}
+	}
+	b.subBoxes = ListToArray(listBoxes, count);
+	printf("nb chars : %d\n", count);
+	return b;
+}
+
+Box SplitWords(ImageGS img, Box b, guchar c);
+
+
+
+void PrintBoxInformations(Box b)
+{
+	printf("x = %d ; y = %d ; w = %d ; h = %d.\n", b.rectangle.x, b.rectangle.y, b.rectangle.width, b.rectangle.height);
+}
+
+
+
+Box SplitLines(ImageGS img, Box b, guchar c)
+{
+	int count = 0;
+	List listBoxes = NULL;
+	printf("Initialization list\n");
+	int inLine = 0; 
+	for (int i = b.rectangle.y; i < b.rectangle.y + b.rectangle.height; i++)
+	{
+		int j = b.rectangle.x;
+		//NOTE : structure d'image en grey scale à faire
+		while (j < b.rectangle.x + b.rectangle.width && img.intensity[i][j] >= c)
+			j++;
+		if (!inLine && j != b.rectangle.x + b.rectangle.width)
+		{
+			inLine = 1;
+			Box btemp;
+			btemp.rectangle.x = b.rectangle.x;
+			btemp.rectangle.y = i;
+			btemp.rectangle.width = b.rectangle.width;
+			listBoxes = Enqueue(listBoxes, btemp, &count);
+			//printf("valeur x : %d\n", listBoxes->value.rectangle.x);
+		}
+		if (inLine && j == b.rectangle.x + b.rectangle.width)
+		{
+			inLine = 0;
+			listBoxes->value.rectangle.height = i - listBoxes->value.rectangle.y;
+		PrintBoxInformations(listBoxes->value);
+		}
+	}
+	b.subBoxes = ListToArray(listBoxes, count);
+	printf("nb lines : %d\n", count);
+	return b;
+}
+
+Box SplitBlocks(ImageGS img, Box b, guchar c);
+
+
+ImageGS BuildImageGS(int width, int height)
+{
+//Initialisation
+	ImageGS img;
+	img.width = width;
+	img.height = height;
+	img.intensity = malloc(sizeof(guchar*) * height);
+	for (int i = 0; i < height; i++)
+		img.intensity[i] = malloc(sizeof(guchar) * width);
+
+//Remplissage
+	for (int j = 0; j < img.width; j++)
+		for (int i = 0; i <= 2; i++)
+			img.intensity[i][j] = 255;
+
+	for (int i = 3; i < img.height/2 - 1; i++)
+	{
+		for (int j = 0; j <= 2; j++)
+			img.intensity[i][j] = 255;
+		for (int j = 3; j < img.width/2 - 1; j++)
+			img.intensity[i][j] = 0;
+		for (int j = img.width/2 - 1; j <= img.width/2 + 1; j++)
+			img.intensity[i][j] = 255;
+		for (int j = img.width/2 + 2; j < img.width - 3; j++)
+			img.intensity[i][j] = 0;
+		for (int j = img.width - 3; j < img.width; j++)
+			img.intensity[i][j] = 255;
+	}
+
+	for (int j = 0; j < img.width; j++)
+		for (int i = img.height/2 - 1; i <= img.height/2 + 1; i++)
+			img.intensity[i][j] = 255;
+
+	for (int i = img.height/2 + 2; i < img.height - 3; i++)
+	{
+		for (int j = 0; j <= 2; j++)
+			img.intensity[i][j] = 255;
+		for (int j = 3; j < img.width/2 - 1; j++)
+			img.intensity[i][j] = 0;
+		for (int j = img.width/2 - 1; j <= img.width/2 + 1; j++)
+			img.intensity[i][j] = 255;
+		for (int j = img.width/2 + 2; j < img.width - 3; j++)
+			img.intensity[i][j] = 0;
+		for (int j = img.width - 3; j < img.width; j++)
+			img.intensity[i][j] = 255;
+	}
+
+	for (int j = 0; j < img.width; j++)
+		for (int i = img.height - 3; i < img.height; i++)
+			img.intensity[i][j] = 255;
+
+	return img;
+}
+
+void PrintImageGS(ImageGS img)
+{
+	for (int i = 0; i < img.height; i++)
+	{
+		for(int j = 0; j < img.width; j++)
+		{
+			if (img.intensity[i][j] >= 128)
+				printf("-");
+			else
+				printf("8");
+		}
+		printf("\n");
+	}
+}
+
+void Test()
+{
+	ImageGS img = BuildImageGS(41,21);
+	PrintImageGS(img);
+	Box box;
+	box.rectangle.x = 0;
+	box.rectangle.y = 0;
+	box.rectangle.width = 41;
+	box.rectangle.height = 21;
+	SplitLines(img, box, 128);
+}
---
+++ src/Utils/split.h
@@ -0,0 +1,45 @@
+#include "image.h"
+typedef struct
+{
+	int x, y, width, height;
+} Rectangle;
+
+typedef struct Box
+{
+	int nbSubBoxes;
+	Rectangle rectangle;
+	struct Box * subBoxes;
+} Box;
+
+typedef struct ElementList
+{
+	Box value;
+	struct ElementList *next;
+} ElementList;
+
+typedef ElementList * List;
+
+List Enqueue(List list, Box b, int * count);//fait
+
+Box * ListToArray(List list, int count);//fait
+
+ImageGS BuildImageGS(int width, int height);//fait mais useless
+
+int HorizontalSpaceInfo(ImageGS img, Box b, guchar c);//fait mais mean value
+
+int VerticalSpaceInfo(ImageGS img, Box b, guchar c);//fait mais mean value
+
+Box SplitChars(ImageGS img, Box b, guchar c);//fait
+
+Box SplitWords(ImageGS img, Box b, guchar c);//A FAIRE
+
+void PrintBoxInformations(Box b);//fait
+
+Box SplitLines(ImageGS img, Box b, guchar c);//fait
+
+Box SplitBlocks(ImageGS img, Box b, guchar c);//A FAIRE
+
+void PrintImageGS(ImageGS img);//fait mais useless
+
+void Test();//à modifier à loisir pour tester
+

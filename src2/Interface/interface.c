08f32329c74bc02eb92bb54e9c5edff209b03865
laurent xu <laurent.xu.epita.fr>
2014-10-27 14:34:56 +0100

    last fix

--- src2/Interface/interface.c
+++ src2/Interface/interface.c
@@ -26,6 +26,8 @@ void Gtk_Initialize(int argc, char *argv[], Image *img)
 	data->boxDetectIndex = 0;
 	data->boxCount = 0;
 	data->previewScale = 1;
+	data->pixbuf = NULL;
+	data->tmp = NULL;
 	gtk_builder_add_from_file(data->builder, uiFilename, &error);
 	g_free(uiFilename);
 
